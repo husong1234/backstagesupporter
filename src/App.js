8ab8ae83f6be4f9bb3e4a4cb806a5e5000b7eb36
Boomkaa <you@example.com>
2019-05-08 15:53:26 +0800

    修改

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Layout } from 'antd';
+import Header from './common/component/Header/index';
+import Slider from './common/component/Slider/index';
 
 class App extends React.Component {
-  render(){
+  render () {
     return (
-      <div>
-        hhh==
-      </div>
+      <Layout>
+        <Header />
+        <Layout>
+          <Slider />
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Layout.Content style={{
+              background: '#fff', padding: 24, marginTop: 30, minHeight: 635,
+            }}
+            >
+              Content
+        </Layout.Content>
+          </Layout>
+        </Layout>
+      </Layout>
     );
   }
 }
