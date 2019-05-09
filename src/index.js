4ecf17334cce1179ecfedacb6a85094409b43f45
Boomkaa <you@example.com>
2019-05-09 11:41:01 +0800

    嗯嗯

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { HashRouter as Router } from 'react-router-dom';
-import store from './store/index';
-import App  from './App';
+// import App  from './App';
+import Login from './page/login/Login';
 
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+  <Login />,
   document.getElementById('root')
 );
